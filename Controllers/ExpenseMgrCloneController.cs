b01cc9c58c6930c46eece038cd420a9cf3171643
Mark Daniel Louwe <mrkdnlph@gmail.com>
2020-05-18 10:41:04 +0800

    Add project files.

---
+++ Controllers/ExpenseMgrCloneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExpenseManagerClone.Controllers
+{
+    public class ExpenseMgrCloneController : Controller
+    {
+        // GET: ExpenseMgrClone
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
